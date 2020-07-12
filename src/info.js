22236539070b859b7edc6221ca6fa0d6216579cc
Scott Carlstrom <scottcarlstrom@hotmail.com>
2020-07-11 20:31:18 -0700

    Update info.js

--- src/info.js
+++ src/info.js
@@ -1,6 +1,6 @@
 const usage = "Usage: crlf-convert [CR|LF|CRLF]";
 
-const help = `Usage: crlf-convert [CR|LF|CRLF]
+const help = String.raw `Usage: crlf-convert [CR|LF|CRLF]
 
 Converts text between newline (\\n), carriage return (\\r), and combined
 "(\\r\\n) line endings.
